a8b295c37b6784191238887a54bdca9a26209541
gregzee <38545800+gregzee@users.noreply.github.com>
2019-03-19 17:58:26 -0400

    Add files via upload

---
+++ ipscrape.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+
+import csv
+
+import requests  # third-party, pip install requests
+
+FIELDNAMES = (
+    'ipAddress',
+    'continentCode',
+    'continentName',
+    'countryCode',
+    'countryName',
+    'stateProv',
+    'city'
+)
+
+
+def output_to_csv(filename, data):
+    with open(filename, 'w') as f:
+        writer = csv.DictWriter(f, fieldnames=FIELDNAMES)
+        writer.writeheader()
+        writer.writerows(data)
+
+
+def get_location_data(ip_to_check):
+    r = requests.get('https://api.ipgeolocation.io/ipgeo-bulk?apiKey=9cec327ae0ba40478779cc152bbf68f9'.format(ip_to_check))
+    r.raise_for_status()
+    return r.json()
+
+if __name__ == '__main__':
+    with open('file_of_ips.txt', 'r') as f:
+        ip_addresses = [line.rstrip() for line in f]
+
+    location_data = [get_location_data(ip) for ip in ip_addresses]
+    output_to_csv('location_data.csv', location_data)
+
+# for hardcoding ip addresses to script
+#
+#if __name__ == '__main__':
+#    ip_addresses = [
+#        '172.217.3.206',   # google.com
+#        '204.79.197.200',  # bing.com
+#        '17.178.96.59',    # apple.com
+#        '104.36.192.183'   # uber.com
+#    ]
+#    location_data = [get_location_data(ip) for ip in ip_addresses]
+#    output_to_csv('location_data.csv', location_data)
---
+++ readme.txt
@@ -0,0 +1,7 @@
+1. Install python3 packages
+2. Put IP Address in file_of_ips.txt
+3. run ipscrape.py w/ file_of_ips.txt in same directory
+4. Will output csv 
+
+This should all work if the correct python packages are installed
+May need requests if fails -- pip install requests
