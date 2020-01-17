63a6b87ede3d6c7411e6ddd90a38028c28bd1f3d
gregzee <38545800+gregzee@users.noreply.github.com>
2020-01-17 10:07:58 -0500

    Update ipscrape.py

--- ipscrape.py
+++ ipscrape.py
@@ -1,27 +1,30 @@
-#!/usr/bin/env python
+#!/usr/bin/env python  
+
 import csv
 import requests
 
 FIELDNAMES = (
-    'ipAddress',
-    'continentCode',
-    'continentName',
-    'countryCode',
+    'ip',
+    'continent_code',
+    'country_name',
+    'country_capital',
     'countryName',
-    'stateProv',
-    'city'
+    'state_prov',
+    'city',
+    'hostname',
+    'organization'
 )
 
 
 def output_to_csv(filename, data):
     with open(filename, 'w') as f:
-        writer = csv.DictWriter(f, fieldnames=FIELDNAMES)
+        writer = csv.DictWriter(f, fieldnames=FIELDNAMES, extrasaction='ignore')
         writer.writeheader()
         writer.writerows(data)
 
 
 def get_location_data(ip_to_check):
-    r = requests.get('https://api.ipgeolocation.io/ipgeo-bulk?apiKey=APIKEY'.format(ip_to_check))
+    r = requests.get(f'https://api.ipgeolocation.io/ipgeo?apiKey=APIKEY&ip={ip_to_check}')
     r.raise_for_status()
     return r.json()
 
@@ -31,15 +34,3 @@ if __name__ == '__main__':
 
     location_data = [get_location_data(ip) for ip in ip_addresses]
     output_to_csv('location_data.csv', location_data)
-
-# for hardcoding ip addresses to script
-#
-#if __name__ == '__main__':
-#    ip_addresses = [
-#        '172.217.3.206',   # google.com
-#        '204.79.197.200',  # bing.com
-#        '17.178.96.59',    # apple.com
-#        '104.36.192.183'   # uber.com
-#    ]
-#    location_data = [get_location_data(ip) for ip in ip_addresses]
-#    output_to_csv('location_data.csv', location_data)
